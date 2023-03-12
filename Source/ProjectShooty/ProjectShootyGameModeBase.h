34878f3f15f8c1c27c701d40e4db2a25968471cb
PChomyszyn <grarl_ppsp@hotmail.com>
2023-03-12 11:30:51 -0600

    Initial commit

---
+++ Source/ProjectShooty/ProjectShootyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectShootyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSHOOTY_API AProjectShootyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
